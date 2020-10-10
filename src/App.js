6d5a2fbb99ef208a5fbc6518ae0c7c98888cd97c
Jessica Waltz <jessicawaltz93@gmail.com>
2020-10-10 13:01:54 -0400

    react-redux basic set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Pokemon from './components/Pokemon.js';
+import Search from './components/Search.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>PKMN Battle Strategy</div>
+      <div>Enter Enemy Team:</div>
+      <Search/>
+      <div>This will calculate the best type advantages to have, it will take into account what type
+        the enemy team is most weak to and what type the enemy team is strongest against to give the 
+        top best types with a score associated with them</div>
+      <Pokemon/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Pokemon.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+  return {
+    pokemon: state.get('selectedPokemon'),
+  }
+}
+
+class Pokemon extends Component {
+  render() {
+    return (
+      <article className="Pokemon">
+        <div className="Sprite">
+          <p>{`http://www.pokestadium.com/sprites/xy/${this.props.pokemon.get('name')}.gif`}</p>
+          <img
+            src={this.props.pokemon.get('sprites').toJS().front_default}
+            crossOrigin="anonymous"
+            ref="sprite"
+            id="sp"
+          />
+        </div>
+        <div className="Detail">
+          <h4 className="Detail-name">
+            {this.props.pokemon.get('name')}
+          </h4>
+        </div>
+      </article>
+    )
+  }
+}
+
+export default connect(mapStateToProps)(Pokemon);
