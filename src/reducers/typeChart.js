f37dfebcac6e016ae0c8e6c7f1178b044e36e434
Jessica Waltz <jessicawaltz93@gmail.com>
2020-10-20 13:25:55 -0400

    added image for background and new category for typeChart

--- src/reducers/typeChart.js
+++ src/reducers/typeChart.js
@@ -24,109 +24,127 @@ const types = {
     "Bug": [
         {
         "weak-to":["Fire","Flying","Rock"],
-        "strong-against":["Grass", "Dark","Psychic"]
+        "strong-against":["Grass", "Dark","Psychic"],
+        "not-very-effective-against": []
         }
     ],
     "Dark":[
         {
             "weak-to":["Bug", "Fairy", "Fighting"],
-            "strong-against":["Ghost", "Psychic"]
+            "strong-against":["Ghost", "Psychic"],
+            "not-very-effective-against": []
         }
     ],
     "Dragon":[
         {
             "weak-to":["Dragon", "Fairy", "Ice"],
-            "strong-against":["Dragon"]
+            "strong-against":["Dragon"],
+            "not-very-effective-against": []
         }
     ],
     "Electric":[
         {
             "weak-to":["Ground"],
-            "strong-against":["Flying", "Water"]
+            "strong-against":["Flying", "Water"],
+            "not-very-effective-against": []
         }
     ],
     "Fairy":[
         {
             "weak-to":["Poison", "Steel"],
-            "strong-against":["Fighting","Dark", "Dragon"]
+            "strong-against":["Fighting","Dark", "Dragon"],
+            "not-very-effective-against": []
         }
     ],
     "Fighting":[
         {
             "weak-to":["Fairy", "Flying", "Psychic"],
-            "strong-against":["Dark", "Ice", "Normal", "Rock", "Steel"]
+            "strong-against":["Dark", "Ice", "Normal", "Rock", "Steel"],
+            "not-very-effective-against": []
         }
     ],
     "Fire":[
         {
             "weak-to":["Ground", "Rock", "Water"],
-            "strong-against":["Bug", "Grass", "Ice", "Steel"]
+            "strong-against":["Bug", "Grass", "Ice", "Steel"],
+            "not-very-effective-against": []
         }
     ],
     "Flying":[
         {
             "weak-to":["Electric", "Ice", "Rock"],
-            "strong-against":["Bug", "Fighting", "Grass"]
+            "strong-against":["Bug", "Fighting", "Grass"],
+            "not-very-effective-against": []
         }
     ],
     "Ghost":[
         {
             "weak-to":["Dark", "Ghost"],
-            "strong-against":["Ghost", "Psychic"]
+            "strong-against":["Ghost", "Psychic"],
+            "not-very-effective-against": []
         }
     ],
     "Grass":[
         {
             "weak-to":["Bug", "Fire", "Flying", "Ice", "Poison"],
-            "strong-against":["Ground", "Rock", "Water"]
+            "strong-against":["Ground", "Rock", "Water"],
+            "not-very-effective-against": []
         }
     ],
     "Ground":[
         {
             "weak-to":["Grass", "Ice", "Water"],
-            "strong-against":["Electric", "Fire", "Poison", "Rock", "Steel"]
+            "strong-against":["Electric", "Fire", "Poison", "Rock", "Steel"],
+            "not-very-effective-against": []
         }
     ],
     "Ice":[
         {
             "weak-to":["Fighting", "Fire", "Rock", "Steel"],
-            "strong-against":["Dragon", "Flying", "Grass", "Ground"]
+            "strong-against":["Dragon", "Flying", "Grass", "Ground"],
+            "not-very-effective-against": []
         }
     ],
     "Normal":[
         {
             "weak-to":["Fighting"],
-            "strong-against":[]
+            "strong-against":[],
+            "not-very-effective-against": []
         }
     ],
     "Poison":[
         {
             "weak-to":["Ground", "Psychic"],
-            "strong-against":["Fairy", "Grass"]
+            "strong-against":["Fairy", "Grass"],
+            "not-very-effective-against": []
         }
     ],
     "Psychic":[
         {
             "weak-to":["Bug", "Dark", "Ghost"],
-            "strong-against":["Fighting", "Poison"]
+            "strong-against":["Fighting", "Poison"],
+            "not-very-effective-against": []
         }
     ],
     "Rock":[
         {
             "weak-to":["Fighting", "Grass", "Ground", "Steel", "Water"],
-            "strong-against":["Bug", "Fire","Flying", "Ice"]
+            "strong-against":["Bug", "Fire","Flying", "Ice"],
+            "not-very-effective-against": []
         }
     ],
     "Steel":[
         {
             "weak-to":["Fighting", "Fire", "Ground"],
-            "strong-against":["Fairy", "Ice", "Rock"]
+            "strong-against":["Fairy", "Ice", "Rock"],
+            "not-very-effective-against": []
         }
     ],
     "Water":[
         {
             "weak-to":["Electric", "Grass"],
-            "strong-against":["Fire", "Ground", "Rock"]
+            "strong-against":["Fire", "Ground", "Rock"],
+            "not-very-effective-against": []
         }
     ]
 }
