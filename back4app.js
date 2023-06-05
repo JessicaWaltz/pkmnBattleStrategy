877571f129b8674aa51e4e86905b5a19b3265427
Jessica Waltz <jessicawaltz93@gmail.com>
2023-06-05 12:14:07 -0400

    back4app fix

--- back4app.js
+++
@@ -1,6 +0,0 @@
-import Parse from 'parse';
-
-Parse.initialize('a14ceba6-2584-4a22-af47-64b9a1cf27ee', 'a14ceba6-2584-4a22-af47-64b9a1cf27ee');
-Parse.serverURL = 'https://parseapi.back4app.com/';
-
-export default Parse;
--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import api from './api/index.js';
 import MyFooter from './components/MyFooter.js';
 import './App.css';
 import '../src/css/PokemonType.css';
+// Import Parse minified version
+import Parse from 'parse/dist/parse.min.js';
+
+// Your Parse initialization configuration goes here
+const PARSE_APPLICATION_ID = 'a14ceba6-2584-4a22-af47-64b9a1cf27ee';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = 'a14ceba6-2584-4a22-af47-64b9a1cf27ee';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 class App extends Component {
   componentDidMount(){
